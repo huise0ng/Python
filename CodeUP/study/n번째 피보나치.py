4b8221c25b9912c387a8bd7a032160c549b340b1
huise0ng <shinhuiseong07@gmail.com>
2023-09-04 16:31:42 +0900

    n번째 파보나치

--- CodeUP/study/n번째 피보나치.py
+++ CodeUP/study/n번째 피보나치.py
@@ -1,7 +1,7 @@
 def func(n):
-    if n==0:
+    if n==0:   # n==0 이라면 결과갑을 1로 반환
         result = 1
-    elif n==1:
+    elif n==1: #n==1 이라면 결과값을 1로 반환
         result = 1
     else:
         result = func(n-1) + func(n-2)
