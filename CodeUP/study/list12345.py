b505c633c0d31277916d60570db29c1b8adf514a
huise0ng <shinhuiseong07@gmail.com>
2023-10-05 09:05:44 +0900

    ADD :: list 수행평가 문제

---
+++ CodeUP/study/list12345.py
@@ -0,0 +1,12 @@
+a = input().split()
+sum = int(0)
+
+for i in range (5) :
+    a[i] = int(a[i])
+    
+for i in range (5) :
+    sum += a[i]
+        
+print(sum)
+
+# --> this problem is change pop
