3ccdb02ff030b62e6afb2fd4ecb7dd51a4b550fc
huise0ng <shinhuiseong07@gmail.com>
2023-10-06 20:54:52 +0900

    codeup-1362

--- CodeUP/study/list12345.py
+++ CodeUP/study/list12345.py
@@ -1,12 +1,12 @@
-def recursive_pop_sum(arr):
+def d(arr):
     if len(arr) == 0:
         return 0
     else:
-        return arr.pop() + recursive_pop_sum(arr)
+        return arr.pop() + d(arr)
 
 a = input().split()
 for i in range(5):
     a[i] = int(a[i])
 
-result = recursive_pop_sum(a)
+result = d(a)
 print(result)
