461fe6108e032019eee194a52cc1a0a56a886820
huise0ng <shinhuiseong07@gmail.com>
2023-10-30 14:38:42 +0900

    ADD :: a b c d e 위치 반대로 바꾸기

---
+++ CodeUP/study/change.py
@@ -0,0 +1,15 @@
+# 1. for문을 이용하는 방법
+
+a = "a b c d e"
+reverse_a = ''
+
+for c in a:
+    reverse_a = c + reverse_a
+
+print(f'name    : {a}')
+print(f'reverse : {reverse_a}')
+
+# 2. abcde
+
+
+#3. s_reverse
